b6d1501a5dd28e53503cc1c97de78ecf03644d53
Mani <karuppasamy.murugesan02@gmail.com>
2025-07-06 20:29:24 +0530

    commit

--- wep/save.php
+++ wep/save.php
@@ -4,6 +4,6 @@ $name=$_POST['username'];
 $email=$_POST['email'];
 $sql="INSERT INTO Students(name,email) values('$name','$email')";
 mysqli_query($my_con, $sql);
-echo "Data saved successfully!";
+echo "<h2>Data saved successfully!</h2>";
 mysqli_close($my_con);
 ?>
